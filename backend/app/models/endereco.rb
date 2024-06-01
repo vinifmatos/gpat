d60575b8ac7c3d6c153daeeebdfd336f2fd96eb6
vinifmatos <viniciusfreire4@gmail.com>
2024-06-01 15:20:55 -0300

    configura atributos aninhados do endereco

--- backend/app/models/endereco.rb
+++ backend/app/models/endereco.rb
@@ -1,6 +1,6 @@
 class Endereco < ApplicationRecord
   belongs_to :cidade
-  belongs_to :local, optional: true
-  belongs_to :fornecedor, optional: true
+  belongs_to :local, optional: true, touch: true
+  belongs_to :fornecedor, optional: true, touch: true
   validates :logradouro, :numero, :bairro, :cep, presence: true
 end
