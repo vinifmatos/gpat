56cf712a5ab9c3dbd24bccc85574525afda82558
vinifmatos <viniciusfreire4@gmail.com>
2024-06-02 15:48:11 -0300

    atualiza index fornecedor para usar o component index

--- frontend/src/app/views/fornecedores/fornecedores-index/fornecedores-index.component.ts
+++ frontend/src/app/views/fornecedores/fornecedores-index/fornecedores-index.component.ts
@@ -1,24 +1,37 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { Fornecedor } from '../../../interfaces/fornecedor';
 import { ImportsModule } from '../../../imports.module';
 import { ApiService } from '../../../services/api.service';
+import { ComponentBase } from '../../../component-base/component-base';
+import { IndexComponent } from '../../shared/index/index.component';
 
 @Component({
   selector: 'app-fornecedores-index',
   standalone: true,
-  imports: [RouterModule, ImportsModule],
+  imports: [RouterModule, ImportsModule, IndexComponent],
   templateUrl: './fornecedores-index.component.html',
   styleUrl: './fornecedores-index.component.scss'
 })
-export class FornecedoresIndexComponent {
+export class FornecedoresIndexComponent extends ComponentBase {
   fornecedores: Fornecedor[]
 
   constructor(
-    private api: ApiService
+    api: ApiService,
+    router: Router,
+    route: ActivatedRoute,
   ) {
-    this.api.get<Fornecedor[]>([this.api.recursos['fornecedores'].rotas.index]).subscribe(res => {
-      this.fornecedores = res.body || []
-    })
+    super(api, router, route)
+    this.api.get<Fornecedor[]>([this.api.recursos['fornecedores'].rotas.index]).subscribe(
+      res => {
+        this.fornecedores = res.body as Fornecedor[]
+        this.carregando = false
+        this.erro = false
+      },
+      res => {
+        this.carregando = false
+        this.erro = false
+      }
+    )
   }
 }
