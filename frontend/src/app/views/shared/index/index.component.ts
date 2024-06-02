7125297bbd3af836dc20a49f7ac7dab228bdc28d
vinifmatos <viniciusfreire4@gmail.com>
2024-06-02 04:48:57 -0300

    implementa o component de index

--- frontend/src/app/views/shared/index/index.component.ts
+++ frontend/src/app/views/shared/index/index.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input, TemplateRef } from '@angular/core';
+import { ImportsModule } from '../../../imports.module';
+import { RouterModule } from '@angular/router';
+import { SpinnerComponent } from '../spinner/spinner.component';
 
 @Component({
   selector: 'app-index',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, ImportsModule, SpinnerComponent],
   templateUrl: './index.component.html',
   styleUrl: './index.component.scss'
 })
 export class IndexComponent {
-
+  @Input() dados_carregados: boolean = false
+  @Input() dados: any
+  @Input() layout: TemplateRef<any>
+  @Input() titulo: string
+  @Input() erro: boolean = false
 }
