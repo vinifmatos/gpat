967f1ef255a1c443190e60bc1313e4a69500b958
vinifmatos <viniciusfreire4@gmail.com>
2024-06-02 20:08:04 -0300

    remoção do css compartilhado para o componente index

--- frontend/src/app/views/locais/locais-index/locais-index.component.ts
+++ frontend/src/app/views/locais/locais-index/locais-index.component.ts
@@ -1,12 +1,36 @@
+import { ActivatedRoute, Router } from '@angular/router';
+import { ApiService } from './../../../services/api.service';
+import { IndexComponent } from './../../shared/index/index.component';
+import { ImportsModule } from './../../../imports.module';
 import { Component } from '@angular/core';
+import { ComponentBase } from '../../../component-base/component-base';
+import { Local } from '../../../interfaces/local';
 
 @Component({
   selector: 'app-locais-index',
   standalone: true,
-  imports: [],
+  imports: [ImportsModule, IndexComponent],
   templateUrl: './locais-index.component.html',
   styleUrl: './locais-index.component.scss'
 })
-export class LocaisIndexComponent {
-
+export class LocaisIndexComponent extends ComponentBase {
+  locais: Local[] = []
+  constructor(
+    api:ApiService,
+    router: Router,
+    route: ActivatedRoute
+  ) {
+    super(api, router, route)
+    api.get<Local[]>([api.recursos['locais'].rotas.index]).subscribe(
+      (res) => {
+        this.locais = res.body as Local[]
+        this.carregando = false
+        this.erro_ao_carregar = false
+      },
+      (res) => {
+        this.carregando = false
+        this.erro_ao_carregar = true
+      }
+    )
+  }
 }
