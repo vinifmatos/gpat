5fe9d169d391e47303f57283510f1a203f6cb59b
vinifmatos <viniciusfreire4@gmail.com>
2024-06-07 16:40:23 -0300

    index do responsavel

--- frontend/src/app/pages/responsaveis/responsaveis-index/responsaveis-index.component.ts
+++ frontend/src/app/pages/responsaveis/responsaveis-index/responsaveis-index.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { ImportsModule } from "../../../imports.module";
+import { IndexComponent } from "../../shared/index/index.component";
+import { ComponentBase } from "../../../component-base/component-base";
+import { ApiService } from "../../../services/api.service";
+import { ActivatedRoute, Router } from "@angular/router";
+import { Responsavel } from "../../../interfaces/responsavel";
 
 @Component({
-  selector: 'app-responsaveis-index',
+  selector: "app-responsaveis-index",
   standalone: true,
-  imports: [],
-  templateUrl: './responsaveis-index.component.html',
-  styleUrl: './responsaveis-index.component.scss'
+  imports: [ImportsModule, IndexComponent],
+  templateUrl: "./responsaveis-index.component.html",
+  styleUrl: "./responsaveis-index.component.scss",
 })
-export class ResponsaveisIndexComponent {
-
+export class ResponsaveisIndexComponent extends ComponentBase {
+  responsaveis: Responsavel[];
+  constructor(api: ApiService, router: Router, route: ActivatedRoute) {
+    super(api, router, route);
+    api.get(api.recursos["responsaveis"].rotas.index).subscribe(
+      (res) => {
+        this.responsaveis = res.body as Responsavel[];
+        this.carregando = false;
+        this.erro_ao_carregar = false;
+      },
+      (res) => {
+        this.carregando = false;
+        this.erro_ao_carregar = true;
+      }
+    );
+  }
 }
--- frontend/src/app/services/api.service.ts
+++ frontend/src/app/services/api.service.ts
@@ -1,101 +1,126 @@
-import { HttpClient, HttpParams, HttpResponse } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import urlJoin from 'url-join';
-import { Recursos } from '../interfaces/recurso';
+import { HttpClient, HttpParams, HttpResponse } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import urlJoin from "url-join";
+import { Recursos } from "../interfaces/recurso";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class ApiService {
-  url_base = '/api'
+  url_base = "/api";
 
   recursos: Recursos = {
     cidades: {
-      nome: 'cidade',
+      nome: "cidade",
       rotas: {
-        index: 'cidades',
-        show: '',
-        get: '',
-        create: '',
-        update: '',
-        delete: ''
-      }
+        index: "cidades",
+        show: "",
+        get: "",
+        create: "",
+        update: "",
+        delete: "",
+      },
     },
     fornecedores: {
-      nome: 'fornecedor',
+      nome: "fornecedor",
       rotas: {
-        index: 'fornecedores',
-        show: 'fornecedores',
-        get: 'fornecedores',
-        create: 'fornecedores',
-        update: 'fornecedores',
-        delete: 'fornecedores'
-      }
+        index: "fornecedores",
+        show: "fornecedores",
+        get: "fornecedores",
+        create: "fornecedores",
+        update: "fornecedores",
+        delete: "fornecedores",
+      },
     },
     grupos: {
-      nome: 'grupo',
+      nome: "grupo",
       rotas: {
-        index: 'grupos',
-        show: 'grupos',
-        get: 'grupos',
-        create: 'grupos',
-        update: 'grupos',
-        delete: 'grupos',
-      }
+        index: "grupos",
+        show: "grupos",
+        get: "grupos",
+        create: "grupos",
+        update: "grupos",
+        delete: "grupos",
+      },
     },
     responsaveis: {
-      nome: 'responsavel',
+      nome: "responsavel",
       rotas: {
-        index: 'responsaveis',
-        show: 'responsaveis',
-        get: 'responsaveis',
-        create: 'responsaveis',
-        update: 'responsaveis',
-        delete: 'responsaveis'
-      }
+        index: "responsaveis",
+        show: "responsaveis",
+        get: "responsaveis",
+        create: "responsaveis",
+        update: "responsaveis",
+        delete: "responsaveis",
+      },
     },
     locais: {
-      nome: 'local',
+      nome: "local",
       rotas: {
-        index: 'locais',
-        show: 'locais',
-        get: 'locais',
-        create: 'locais',
-        update: 'locais',
-        delete: 'locais'
-      }
+        index: "locais",
+        show: "locais",
+        get: "locais",
+        create: "locais",
+        update: "locais",
+        delete: "locais",
+      },
     },
     patrimonios: {
-      nome: 'patrimonio',
+      nome: "patrimonio",
       rotas: {
-        index: 'patrimonios',
-        show: 'patrimonios',
-        get: 'patrimonios',
-        create: 'patrimonios',
-        update: 'patrimonios',
-        delete: 'patrimonios'
-      }
+        index: "patrimonios",
+        show: "patrimonios",
+        get: "patrimonios",
+        create: "patrimonios",
+        update: "patrimonios",
+        delete: "patrimonios",
+      },
     },
+  };
+
+  constructor(private http: HttpClient) {}
+
+  get<T>(
+    path: string | string[],
+    params: any = {}
+  ): Observable<HttpResponse<T>> {
+    if (!Array.isArray(path)) path = [path];
+    return this.http.get<T>(
+      urlJoin(this.url_base, ...path.map((p) => String(p))),
+      { observe: "response", params: params }
+    );
   }
 
-  constructor(
-    private http: HttpClient
-  ) { }
-
-  get<T>(path: string[], params: any = {}): Observable<HttpResponse<T>> {
-    return this.http.get<T>(urlJoin(this.url_base, ...path.map((p => String(p)))), { observe: 'response', params: params })
+  create<T>(
+    path: string | string[],
+    payload: any
+  ): Observable<HttpResponse<T>> {
+    if (!Array.isArray(path)) path = [path];
+    return this.http.post<T>(
+      urlJoin(this.url_base, ...path.map((p) => String(p))),
+      payload,
+      { observe: "response" }
+    );
   }
 
-  create<T>(path: string[], payload: any): Observable<HttpResponse<T>> {
-    return this.http.post<T>(urlJoin(this.url_base, ...path.map((p => String(p)))), payload, { observe: 'response' })
+  update<T>(
+    path: string | string[],
+    payload: any
+  ): Observable<HttpResponse<T>> {
+    if (!Array.isArray(path)) path = [path];
+    return this.http.put<T>(
+      urlJoin(this.url_base, ...path.map((p) => String(p))),
+      payload,
+      { observe: "response" }
+    );
   }
 
-  update<T>(path: string[], payload: any): Observable<HttpResponse<T>> {
-    return this.http.put<T>(urlJoin(this.url_base, ...path.map((p => String(p)))), payload, { observe: 'response' })
-  }
-
-  delete<T>(path: string[]): Observable<HttpResponse<T>> {
-    return this.http.delete<T>(urlJoin(this.url_base, ...path.map((p => String(p)))), { observe: 'response' })
+  delete<T>(path: string | string[]): Observable<HttpResponse<T>> {
+    if (!Array.isArray(path)) path = [path];
+    return this.http.delete<T>(
+      urlJoin(this.url_base, ...path.map((p) => String(p))),
+      { observe: "response" }
+    );
   }
 }
