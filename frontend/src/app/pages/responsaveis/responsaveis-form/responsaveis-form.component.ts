e03188ee668f7bf8e91bdfb0f68889dbaf963586
vinifmatos <viniciusfreire4@gmail.com>
2024-06-07 16:55:31 -0300

    move css do checkbox para o escopo global

--- frontend/src/app/pages/responsaveis/responsaveis-form/responsaveis-form.component.ts
+++ frontend/src/app/pages/responsaveis/responsaveis-form/responsaveis-form.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from "@angular/forms";
+import { FormComponentBase } from "../../../component-base/form-component-base";
+import { ApiService } from "../../../services/api.service";
+import { Responsavel } from "./../../../interfaces/responsavel";
+import { Component } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { StringService } from "../../../services/string.service";
+import { ImportsModule } from "../../../imports.module";
+import { FormComponent } from "../../shared/form/form.component";
 
 @Component({
-  selector: 'app-responsaveis-form',
+  selector: "app-responsaveis-form",
   standalone: true,
-  imports: [],
-  templateUrl: './responsaveis-form.component.html',
-  styleUrl: './responsaveis-form.component.scss'
+  imports: [ImportsModule, FormComponent],
+  templateUrl: "./responsaveis-form.component.html",
+  styleUrl: "./responsaveis-form.component.scss",
 })
-export class ResponsaveisFormComponent {
+export class ResponsaveisFormComponent extends FormComponentBase {
+  override campos: Responsavel;
+  constructor(
+    api: ApiService,
+    fb: FormBuilder,
+    route: ActivatedRoute,
+    router: Router,
+    strs: StringService
+  ) {
+    let campos = {
+      id: null,
+      cpf: "",
+      nome: "",
+      ativo: true,
+    };
+    super(api, campos, fb, route, router, api.recursos["responsaveis"], strs);
+  }
 
+  protected override build_form(): FormGroup<any> {
+    return this.fb.group({
+      id: this.campos.id,
+      cpf: this.campos.cpf,
+      nome: this.campos.nome,
+      ativo: this.campos.ativo,
+    });
+  }
 }
