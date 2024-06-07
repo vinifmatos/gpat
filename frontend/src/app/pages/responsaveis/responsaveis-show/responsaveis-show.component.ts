bc1e2f834af18c25ab5a28e633de3e52dc651791
vinifmatos <viniciusfreire4@gmail.com>
2024-06-07 17:16:51 -0300

    show do responsavel

--- frontend/src/app/pages/responsaveis/responsaveis-show/responsaveis-show.component.ts
+++ frontend/src/app/pages/responsaveis/responsaveis-show/responsaveis-show.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Responsavel } from "../../../interfaces/responsavel";
+import { ApiService } from "../../../services/api.service";
+import { ActivatedRoute, Router } from "@angular/router";
+import { ComponentBase } from "../../../component-base/component-base";
+import { ImportsModule } from "../../../imports.module";
+import { ShowComponent } from "../../shared/show/show.component";
 
 @Component({
-  selector: 'app-responsaveis-show',
+  selector: "app-responsaveis-show",
   standalone: true,
-  imports: [],
-  templateUrl: './responsaveis-show.component.html',
-  styleUrl: './responsaveis-show.component.scss'
+  imports: [ImportsModule, ShowComponent],
+  templateUrl: "./responsaveis-show.component.html",
+  styleUrl: "./responsaveis-show.component.scss",
 })
-export class ResponsaveisShowComponent {
+export class ResponsaveisShowComponent extends ComponentBase {
+  responsavel: Responsavel;
 
+  constructor(api: ApiService, router: Router, route: ActivatedRoute) {
+    super(api, router, route);
+    this.route.params.subscribe(() => {
+      this.carregando = true;
+      this.api
+        .get<Responsavel>([
+          this.api.recursos["responsaveis"].rotas.get,
+          this.route.snapshot.paramMap.get("id") as string,
+        ])
+        .subscribe(
+          (res) => {
+            this.responsavel = res.body as Responsavel;
+            this.carregando = false;
+            this.erro_ao_carregar = false;
+          },
+          (res) => {
+            this.carregando = false;
+            this.erro_ao_carregar = true;
+          }
+        );
+    });
+  }
 }
